8dff2b1b0075b10d637a748ebdee371aed6a3a82
Frans Paxi <frans.paxi@upeu.edu.pe>
2024-04-16 15:25:22 -0500

    laptop dell

--- scraping-sinfronteras.py
+++ scraping-sinfronteras.py
@@ -17,10 +17,11 @@ html = BeautifulSoup(response.content, 'html.parser')
 seccion = html.find_all('li', class_='infinite-post')
 #print(secciones)
 
+
 # Iteramos sobre cada sección para obtener el título y la descripción
 for element in seccion:
     # Obtenemos el titulo que esta dentro de la etiqueta h2
-    titulo = element.find('h2').text
+    titulo = element.find('h2').tex
     # Obtenemos la descripción que esta dentro de la etiqueta p
     descripcion = element.find('p').text
     # Obtenemos el src que esta dentro de la etiqueta img
@@ -43,7 +44,7 @@ with codecs.open('noticias_sf.csv', 'w', 'utf-8-sig') as csvfile:
     # csvWriter es un objeto que nos permite escribir en el archivo csv
     csvWriter = csv.writer(csvfile, delimiter='|')
     # Escribimos la cabecera del archivo csv
-    csvWriter.writerow(['Fecha', 'Titulo', 'Resumen'])
+    csvWriter.writerow(['Fecha', 'Titulo', 'Resumen', 'Resumen Completo'])
 
     # Iteramos sobre cada sección para obtener el título, la descripción y la fecha
     for secciones in seccion :
@@ -52,6 +53,23 @@ with codecs.open('noticias_sf.csv', 'w', 'utf-8-sig') as csvfile:
         # Obtenemos la descripción que esta dentro de la etiqueta p
         resumen = secciones.find("p").text
 
+        #------------------------
+        href_text = secciones.find("a").get("href")
+        href_response = requests.get(href_text)
+        href_html = BeautifulSoup(href_response.content, 'html.parser')
+        # Buscamos el resumen completo
+        resumen_completo = href_html.find_all('div', id='content-main')
+        for h4_resumen in resumen_completo:
+           descripcion_h4 = h4_resumen.find('h4').text
+           print("descripcion_h4",descripcion_h4)
+           for p_resumen in h4_resumen.find_all('p'):
+               descripcion_p = p_resumen.text
+               print("descripcion_p",descripcion_p)
+               for div_resumen in h4_resumen.find_all('div'):
+                   descripcion_div = div_resumen.text
+                   print("descripcion_div",descripcion_div)
+        resumen_h_p = descripcion_h4 + descripcion_p + descripcion_div
+        print(resumen_h_p)
         # Obtenemos el enlace que esta dentro de la etiqueta a
         enlace = secciones.find("a")
         # Obtenemos el href que esta dentro de la etiqueta a
@@ -60,5 +78,7 @@ with codecs.open('noticias_sf.csv', 'w', 'utf-8-sig') as csvfile:
         f_math = re.search(r'\d{4}/\d{2}/\d{2}', c_href)
         # Se encuen
         dfecha = f_math.group()
-        csvWriter.writerow([dfecha, titulo, resumen])
+        csvWriter.writerow([dfecha, titulo, resumen, resumen_h_p])
+   
+    
     
